6c02a8fb849a0200cf257026553627eda9ac69dc
Sahir Nambiar <sahirnambiar@gmail.com>
2018-11-06 21:10:50 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function  shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase);
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase);
+}
+
+function sayHiToGrandma(string) {
+  var lc = "hello!";
+  if(string === lc) {
+    return "I can't hear you!"
+  }
+  else {
+    return 'I love you, too.'
+  }
+}
